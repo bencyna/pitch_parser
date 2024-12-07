fbc895dfc237d71f9d8bd323eec50f4e70f88c3c
Ben Cyna <benjamincyna@gmail.com>
2024-12-07 14:38:40 -0500

    printing the basics

---
+++ test.cpp
@@ -0,0 +1,3 @@
+#include<iostream>
+using namespace std;
+int main() {  int i = 0; char j = '5'; printf("%d\n", (i+j)); return 0; } 
---
+++ test.py
@@ -0,0 +1,6 @@
+def type_check(a):
+  p = 7
+  return (p + a)
+
+print(type_check('4'))
+  
